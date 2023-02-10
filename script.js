550123599cd86b15e507d53b55a667ccc11b9da4
Caylin Vu <caylinvu@gmail.com>
2023-02-10 12:45:07 -0600

    Set custom validity on password confirmation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const allInputs = document.querySelectorAll("input");
 const password = document.querySelector(".pwd");
 const passwordConfirmation = document.querySelector(".pwd-conf");
 const pwdSpan = document.querySelector(".pwd-span");
+const submitBtn = document.querySelector("button");
 
 // add "text-present" class to inputs after text has been entered (to only add invalidstyling after text has been entered)
 allInputs.forEach((input) => input.addEventListener('keyup', () => {
@@ -14,15 +15,24 @@ allInputs.forEach((input) => input.addEventListener('keyup', () => {
 passwordConfirmation.addEventListener('keyup', () => {
     if (passwordConfirmation.value != password.value) {
         passwordConfirmation.classList.add("no-match");
+        passwordConfirmation.setCustomValidity('Passwords do not match');
     } else if (passwordConfirmation.value == password.value) {
         passwordConfirmation.classList.remove("no-match");
+        passwordConfirmation.setCustomValidity('');
     }
 });
 
 password.addEventListener('keyup', () => {
     if (passwordConfirmation.value != password.value) {
         passwordConfirmation.classList.add("no-match");
+        passwordConfirmation.setCustomValidity('Passwords do not match');
     } else if (passwordConfirmation.value == password.value) {
         passwordConfirmation.classList.remove("no-match");
+        passwordConfirmation.setCustomValidity('');
     }
+});
+
+// add "submitted" class to inputs when press submit button (to show invalid styling if submit button is pressed)
+submitBtn.addEventListener('click', () => {
+    allInputs.forEach((input) => input.classList.add("submitted"));
 });
