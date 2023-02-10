88b148e1e3a5b5326cbbd1b62fe0b5a5cd1e4d00
Caylin Vu <caylinvu@gmail.com>
2023-02-10 14:53:42 -0600

    Clean up code

--- script.js
+++ script.js
@@ -4,35 +4,30 @@ const passwordConfirmation = document.querySelector(".pwd-conf");
 const pwdSpan = document.querySelector(".pwd-span");
 const submitBtn = document.querySelector("button");
 
-// add "text-present" class to inputs after text has been entered (to only add invalidstyling after text has been entered)
+// validates password confirmation
+function validatePassword() {
+    if (passwordConfirmation.value != password.value) {
+        passwordConfirmation.classList.add("no-match");
+        passwordConfirmation.setCustomValidity('Passwords do not match');
+    } else if (passwordConfirmation.value == password.value) {
+        passwordConfirmation.classList.remove("no-match");
+        passwordConfirmation.setCustomValidity('');
+    }
+}
+
+passwordConfirmation.addEventListener('keyup', validatePassword);
+password.addEventListener('keyup', validatePassword);
+
+// adds invalid styling to inputs only after text has been entered
 allInputs.forEach((input) => input.addEventListener('keyup', () => {
     if (input.value.length > 0) {
         input.classList.add("text-present");
     }
 }));
 
-// add/remove "no-match" class when typing in password (to add/remove password confirmation text if passwords match or don't match)
-passwordConfirmation.addEventListener('keyup', () => {
-    if (passwordConfirmation.value != password.value) {
-        passwordConfirmation.classList.add("no-match");
-        passwordConfirmation.setCustomValidity('Passwords do not match');
-    } else if (passwordConfirmation.value == password.value) {
-        passwordConfirmation.classList.remove("no-match");
-        passwordConfirmation.setCustomValidity('');
-    }
-});
 
-password.addEventListener('keyup', () => {
-    if (passwordConfirmation.value != password.value) {
-        passwordConfirmation.classList.add("no-match");
-        passwordConfirmation.setCustomValidity('Passwords do not match');
-    } else if (passwordConfirmation.value == password.value) {
-        passwordConfirmation.classList.remove("no-match");
-        passwordConfirmation.setCustomValidity('');
-    }
-});
-
-// add "submitted" class to inputs when press submit button (to show invalid styling if submit button is pressed)
+// keeps invalid styling on invalid inputs after submitting
 submitBtn.addEventListener('click', () => {
     allInputs.forEach((input) => input.classList.add("submitted"));
 });
+
